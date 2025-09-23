954d750ee7648de74f1061b33ba8e6b5a0c43fb2
melon.li <d.q.li@foxmail.com>
2025-09-23 11:16:49 -0400

    pre-oh

--- computer-graphics-ray-tracing/src/blinn_phong_shading.cpp
+++ computer-graphics-ray-tracing/src/blinn_phong_shading.cpp
@@ -13,6 +13,21 @@ Eigen::Vector3d blinn_phong_shading(
 {
   ////////////////////////////////////////////////////////////////////////////
   // Replace with your code here:
+  Eigen::Vector3d point = ray.origin + t * ray.direction;
+  for (int i = 0; i < lights.size(); i++) {
+    Eigen::Vector3d temp_d;
+    double temp_max_t;
+    Ray temp_ray;
+    lights[i]->direction(point, temp_d, temp_max_t);
+    temp_ray.origin = point;
+    temp_ray.direction = temp_d;
+    int temp_hit_id;
+    double temp_t;
+    Eigen::Vector3d temp_n;
+    if (!first_hit(temp_ray,0,objects,temp_hit_id,temp_t,temp_n)) {// if didn'y hit anything
+
+    }
+  }
   return Eigen::Vector3d(0,0,0);
   ////////////////////////////////////////////////////////////////////////////
 }
